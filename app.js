03d3cb291bfca08b3369cde8a24da0f340ced9bf
yuval hadar <yuvalhad12@gmail.com>
2023-07-17 17:44:30 +0300

    now work only on remote server

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ passport.use(User.createStrategy());
 passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://secrets-app-t294.onrender.com/auth/google/secrets",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
