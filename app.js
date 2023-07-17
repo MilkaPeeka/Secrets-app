1a51cde4f7b67c72d0a8e1afbef75f272f6be0e3
yuval hadar <yuvalhad12@gmail.com>
2023-07-17 15:02:32 +0300

    can sign in and sign and log out with google but need to research more about what happens in here

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require("mongoose");
 const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 
 mongoose.connect("mongodb+srv://Yuval:Test-123@cluster0.dcwwtsq.mongodb.net/secretDB");
@@ -29,18 +31,42 @@ app.use(passport.session());
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String,
+    googleId: String
   });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.use(new GoogleStrategy({
+    clientID:     process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    passReqToCallback   : true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
 
-
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, {
+        id: user.id,
+      });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
 app.get('/sign_up', (req, res) => {
     res.render('sign-up');
 });
@@ -85,13 +111,14 @@ app.post('/sign_in', (req, res) => {
 
 
 app.get('/secrets', (req, res) => {
+    console.log(req.isAuthenticated());
     if (req.isAuthenticated()) 
         res.render('secrets');
     else 
         res.redirect('/sign_in');
 });
 
-app.get('/log_out', (req, res) => {
+app.get('/sign_out', (req, res) => {
     req.logout((err) => {
       if (err) {
         console.log(err);
@@ -99,4 +126,19 @@ app.get('/log_out', (req, res) => {
       res.redirect('/secrets');
     });
   });
+
+
+
+app.get("/auth/google",
+passport.authenticate('google', { scope: ["profile"] })
+);
+
+app.get("/auth/google/secrets",
+  passport.authenticate('google', { failureRedirect: "/sign_in" }),
+  function(req, res) {
+    // Successful authentication, redirect to secrets.
+    res.redirect("/secrets");
+  });
+
+
 app.listen('3000', () => console.log('listening on 3000'));
